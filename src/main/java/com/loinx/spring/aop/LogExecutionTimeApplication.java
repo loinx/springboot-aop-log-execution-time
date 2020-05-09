465b8cb7c21302318f4ece76ea125866df4e57e6
LoiNX <loinx90@gmail.com>
2020-05-09 15:39:10 +0700

    Initialize the project

---
+++ src/main/java/com/loinx/spring/aop/LogExecutionTimeApplication.java
@@ -0,0 +1,13 @@
+package com.loinx.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogExecutionTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogExecutionTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loinx/spring/aop/LogExecutionTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loinx.spring.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogExecutionTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
