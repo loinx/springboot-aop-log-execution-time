5e13c1ba4edd2988c93d4f4181a0910cde33b1ec
LoiNX <loinx90@gmail.com>
2020-05-10 10:31:36 +0700

    Add source generated by mapstruct

--- src/main/java/com/loinx/spring/aop/controller/UserController.java
+++ src/main/java/com/loinx/spring/aop/controller/UserController.java
@@ -35,5 +35,5 @@ public class UserController {
 				: ResponseEntity.notFound().build();
 		return response;
 	}
-
+	
 }
--- src/main/java/com/loinx/spring/aop/service/UserServiceImpl.java
+++ src/main/java/com/loinx/spring/aop/service/UserServiceImpl.java
@@ -43,5 +43,4 @@ public class UserServiceImpl implements UserService {
 			return null;
 		})));
 	}
-
 }
